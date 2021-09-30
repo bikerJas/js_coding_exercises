458b328a05caec7002ba00e15e6edf262690b6f5
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-09-30 14:04:11 +0100

    Implemented getFillings;isFromManchester;getBusNumbers;countSheep

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,50 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
-}
+  fillings = [];
+  for (var x = 0; x < sandwich.fillings.length; x++){
+    fillings[x] = sandwich.fillings[x];
+  }
+  return fillings;
+} 
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return (person.city == "Manchester");
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  switch (people) {
+    case (people <= 40):
+      buses = 1;
+      break;
+    case (people <= 80):
+      buses = 2;
+      break;
+    case (people <= 120):
+      buses = 3;
+      break;
+    default:
+      buses = Math.ceil(people/40);    
+  }
+  return buses;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  num = 0;
+  for (var x = 0; x < arr.length; x++) {
+    if (arr[x] == "sheep") {
+      num += 1;
+    }
+  }
+  return num;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  code = person.postCode;
+  return person.postCode;
 }
 
 module.exports = {
