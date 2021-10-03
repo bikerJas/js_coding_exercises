b510b5b37735ef310bac4c0cf573c673189d66e3
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-10-03 16:48:53 +0100

    Implemented exercise005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -18,33 +18,82 @@ const findNextNumber = (nums, n) => {
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
-};
+  const digits = {0:0,1:0};
 
-const reverseNumber = n => {
+  for (let x = 0; x < str.length; x++) {
+    let digit = str[x];
+    if (digits[digit]) {
+      digits[digit]++;
+    } else {
+      digits[digit] = 1;
+    } 
+  }
+  return digits;
+};
+  
+ const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  let reversed = n.toString().split("").reverse().join("");
+  reversed = parseInt(reversed);
+  return reversed;
 };
 
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  let totalArrays = 0;
+  arrs.forEach(function (subArray){
+    const innerArray = subArray;
+    innerArray.forEach(function (total){
+      totalArrays += total;
+    });
+  
+});
+  return totalArrays;
 };
 
 const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let first = "";
+  let last = "";
+  if (arr.length > 1){
+    first = arr.shift();
+    last = arr.pop();
+    arr.push(first);
+    arr.unshift(last);
+    return arr;
+  } else {
+    return arr;
+  }
 };
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  let foundit = false;
+  let items = Object.values(haystack);
+  items.forEach(function (findWord){
+    if(findWord.toString().toLowerCase().includes(searchTerm.toLowerCase())){
+      foundit = true;
+    }
+  });
+  
+  return foundit;
 };
 
 const getWordFrequencies = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  const words = {};
+  let splitWords = str.replace(/[^a-zA-Z ]/g, "").split(" ");
+
+  for (let x = 0; x < splitWords.length; x++) {
+    let word = splitWords[x].toLowerCase();
+    if (words[word]) {
+      words[word]++;
+    } else {
+      words[word] = 1;
+    } 
+  }
+  return words;
 };
 
 module.exports = {
