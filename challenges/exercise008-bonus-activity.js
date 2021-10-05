c2b397e9926e9a510cf9960cbfba1cc6717240f5
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-10-05 15:54:39 +0100

    Refactored exercise008

--- challenges/exercise008-bonus-activity.js
+++ challenges/exercise008-bonus-activity.js
@@ -2,26 +2,22 @@
 
 function printColours(colours){
     if (!colours) throw new Error("colour is required");
-    //const allColours = [...colours];
     return [...colours];
 }
 
 function shoppingList(list, newItem){
     if (!list) throw new Error("list is required");
     if (!newItem) throw new Error("newItem is required");
-    //const newShoppingList = [...list, ...newItem];
     return [...list, ...newItem];
 }
 
 function highestNumber(numbers){
     if (!numbers) throw new Error("numbers are required");
-    //const maxNum = Math.max(...numbers);
     return Math.max(...numbers);
 }
 
 function splitThatString(string){ 
     if (!string) throw new Error("string is required");
-    //const splitString = [...string];
     return [...string];
 }
 
